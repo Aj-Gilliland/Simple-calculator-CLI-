c28b957bb94d2d388dac54c37774284f4dd621f0
AJ Gilliland <manuelgillilandiv@gmail.com>
2024-01-18 14:36:23 -0600

    will be tweaking in the coming future

---
+++ Modula1.java
@@ -0,0 +1,91 @@
+import java.util.Scanner;
+
+public class Modula1 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print(""
+                + " __________\r\n"
+                + "| ________ |\r\n"
+                + "||12345678||\r\n"
+                + "|\"\"\"\"\"\"\"\"\"\"|\r\n"
+                + "|[M|#|C][-]|\r\n"
+                + "|[7|8|9][+]|\r\n"
+                + "|[4|5|6][x]|\r\n"
+                + "|[1|2|3][%]|\r\n"
+                + "|[.|O|:][=]|\r\n"
+                + "\"==========\"\n");
+        System.out.print("Welcome to the simple calculator\n");
+        System.out.print("you can enter >Exit< to leave\n");
+        while (true) {
+            System.out.print("Enter a simple expression using * / + - \n: ");
+            String userInput = scanner.nextLine();
+            if (userInput.equalsIgnoreCase("exit")) {
+                System.out.print("Have a nice day and thank you for choosing the simple method");
+                scanner.close(); // this is here to prevent data leaks?
+                break;
+            } else if (countOperators(userInput) > 1 || !userInput.matches("^[0-9]+[-+*/]+[0-9]+$")) {
+                System.out.println("Sorry, simple equations only");
+            } else if (countOperators(userInput) == 1 && userInput.matches("^[0-9]+[-+*/]+[0-9]+$")) {
+                float result = handleInput(userInput);
+                System.out.println(result);
+            }
+        }
+    }
+
+    private static int countOperators(String input) {
+        int count = 0;
+        for (char c : input.toCharArray()) {
+            if (c == '*' || c == '-' || c == '+' || c == '/') {
+                count += 1;
+            }
+        }
+        return count;
+    }
+
+    private static float handleInput(String input) {
+        float result = 0.0f;
+
+        // Split the input string based on operators
+        String[] equationParts = input.split("\\s*[-+*/]\\s*");
+
+        if (equationParts.length != 2) {
+            System.out.println("Invalid equation format.");
+            return result;
+        }
+
+        float num1 = Float.parseFloat(equationParts[0]);
+        float num2 = Float.parseFloat(equationParts[1]);
+
+        // Determine the operator by turning everything thats not a op into white space
+        char operator = input.replaceAll("[^-+*/]", "").charAt(0);
+
+        // Perform the corresponding operation
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                // Check for division by zero
+                if (num2 != 0) {
+                    result = num1 / num2;
+                } else {
+                    System.out.println("Cannot divide by zero.");
+                    result = 123456789.123456789F;
+                    return result; // Early return to avoid further processing
+                }
+                break;
+            default:
+                System.out.println("Invalid operator.");
+                return result; // Early return for invalid operator
+        }
+        return result; // Add this return statement for the default case
+    }
+}
